fdcab526c1460392997a96da0d6a975a1d275555
Andresargote <andresargote2019@gmail.com>
2022-08-08 18:07:23 +0200

    Completed the first version of my personal site

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -4,7 +4,12 @@ export function ContactMe() {
   const { t } = useTranslation('home');
 
   return (
-    <a className='px-4 py-3 text-white bg-black shadow-lg rounded-3xl shadow-green-500/40'>
+    <a
+      className='px-4 py-3 text-white bg-black shadow-lg rounded-3xl shadow-green-500/40'
+      target='_blank'
+      rel='noopener noreferrer'
+      href='mailto:contact@andresargote.com'
+    >
       {t('contact')} 📨
     </a>
   );
--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 
 export function Select({ options }) {
   const router = useRouter();
+  const [locale, setLocale] = useState(router.locale);
 
   const handleChangeLanguage = (e) => {
+    setLocale(e.target.value);
     router.push(router.pathname, router.pathname, {
       locale: e.target.value,
     });
@@ -13,6 +16,7 @@ export function Select({ options }) {
     <select
       className='absolute w-[70px] px-4 py-2 text-gray-900 bg-gray-100 text-lg rounded-xl top-6 right-6 sm:w-fit sm:text-sm'
       onChange={handleChangeLanguage}
+      value={locale}
     >
       {options.map((option) => (
         <option key={option.id} value={option.value}>
